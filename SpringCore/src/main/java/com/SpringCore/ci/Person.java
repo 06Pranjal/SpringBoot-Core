f0e9ad5e5f005272cd13ac3027899a1daf1ec6d8
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-06-21 16:36:37 +0530

    constructor Injection

--- SpringCore/src/main/java/com/SpringCore/ci/Person.java
+++ SpringCore/src/main/java/com/SpringCore/ci/Person.java
@@ -3,16 +3,18 @@ package com.SpringCore.ci;
 public class Person {
 	private String name;
 	private int personid;
+	private Certi certi;
 	
-	public Person(String name,int personId) {
+	public Person(String name,int personId,Certi certi) {
 		this.name=name;
 		this.personid=personId;
+		this.certi=certi;
 	}
 
 	@Override
 	public String toString() {
 		
-		return this.name+" : "+this.personid;
+		return this.name+" : "+this.personid+"{ "+this.certi.name+" }";
 	}
 	
 	
