7677f00cee7bd736e1273a2e62cc20b42dbac1ec
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-06-22 12:11:05 +0530

    Implementing Bean Lifecycle methods using Interface

---
+++ SpringCore/src/main/java/com/SpringCore/lifecycle2/Test.java
@@ -0,0 +1,32 @@
+package com.SpringCore.lifecycle2;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/SpringCore/lifecycle2/config.xml");
+		
+		//registering shutdown hook
+		context.registerShutdownHook(); 
+		
+		// Samosa s1=(Samosa) context.getBean("s1");
+		// System.out.println(s1);
+		
+		
+		
+		
+		System.out.println("++++++++++++++++++++++++++");
+
+		Pepsi p1=(Pepsi) context.getBean("p1");
+		System.out.println(p1);
+
+		
+		
+
+
+	}
+
+}
