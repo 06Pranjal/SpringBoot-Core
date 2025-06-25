c98863e4e6dc2fac55c0a2fd30725cb275e245ee
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-06-25 12:53:08 +0530

    Qualifier Annotations

--- SpringCore/src/main/java/com/SpringCore/auto/wire/annotation/Emp.java
+++ SpringCore/src/main/java/com/SpringCore/auto/wire/annotation/Emp.java
@@ -1,9 +1,11 @@
 package com.SpringCore.auto.wire.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
 	@Autowired
+	@Qualifier("address")
 	private Address address;
 
 	@Override
