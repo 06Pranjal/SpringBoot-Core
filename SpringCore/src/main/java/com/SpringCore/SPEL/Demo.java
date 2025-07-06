8eda97b3306412a40180a579db3da9009457d2f1
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-07-06 12:53:35 +0530

    Static functions using Spel

--- SpringCore/src/main/java/com/SpringCore/SPEL/Demo.java
+++ SpringCore/src/main/java/com/SpringCore/SPEL/Demo.java
@@ -2,7 +2,6 @@ package com.SpringCore.SPEL;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 
 @Component
 public class Demo {
@@ -10,6 +9,14 @@ public class Demo {
 	private int x;
 	@Value("#{10+11}")
 	private int y;
+	@Value("#{T(java.lang.Math).pow(25,2)}")
+	private double z;
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	public int getX() {
 		return x;
 	}
@@ -24,7 +31,8 @@ public class Demo {
 	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + "]";
 	}
+	
 	
 }
