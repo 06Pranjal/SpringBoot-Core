7132a5e030a2fbedc2598c6c9b8312a9adc0cd86
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-07-06 13:36:19 +0530

    Removing XML config

---
+++ SpringCore/src/main/java/com/SpringCore/javaConfig/Samosa.java
@@ -0,0 +1,8 @@
+package com.SpringCore.javaConfig;
+
+public class Samosa {
+	public void display() {
+		System.out.println("My price is little high");
+
+	}
+}
---
+++ SpringCore/src/main/java/com/SpringCore/javaConfig/Student.java
@@ -0,0 +1,26 @@
+package com.SpringCore.javaConfig;
+
+
+public class Student {
+	
+	private Samosa samosa;
+	
+	public Student(Samosa samosa) {
+		super();
+		this.samosa = samosa;
+	}
+
+	public Samosa getSamosa() {
+		return samosa;
+	}
+
+	public void setSamosa(Samosa samosa) {
+		this.samosa = samosa;
+	}
+
+	public void Study() {
+	this.samosa.display();	
+	System.out.println("Student is Studying");
+
+}
+}
