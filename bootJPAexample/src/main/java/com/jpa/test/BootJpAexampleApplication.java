9ec98cf66dc509feb294a1089e69b4449b3ae8cf
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-08-05 01:27:14 +0530

    SpringBoot JPA

--- bootJPAexample/src/main/java/com/jpa/test/BootJpAexampleApplication.java
+++ bootJPAexample/src/main/java/com/jpa/test/BootJpAexampleApplication.java
@@ -2,12 +2,26 @@ package com.jpa.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entites.User;
 
 @SpringBootApplication
 public class BootJpAexampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootJpAexampleApplication.class, args);
+		
+		ApplicationContext context=SpringApplication.run(BootJpAexampleApplication.class, args);
+		UserRepository userRepository=context.getBean(UserRepository.class);
+		User user =new User();
+		user.setName("Pranjal Singh");
+		user.setCity("Delhi");
+		user.setStatus("Java programmer");
+		
+		 User user1=userRepository.save(user);
+		 System.out.println(user1);
+
 	}
 
 }
