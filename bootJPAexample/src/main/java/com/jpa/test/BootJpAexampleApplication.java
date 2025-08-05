013617dcf43e1166f2ae0464945e759dcad39438
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-08-05 09:17:29 +0530

    User defined to add data

--- bootJPAexample/src/main/java/com/jpa/test/BootJpAexampleApplication.java
+++ bootJPAexample/src/main/java/com/jpa/test/BootJpAexampleApplication.java
@@ -1,5 +1,9 @@
 package com.jpa.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,14 +14,21 @@ import com.jpa.test.entites.User;
 @SpringBootApplication
 public class BootJpAexampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
 		
 		ApplicationContext context=SpringApplication.run(BootJpAexampleApplication.class, args);
 		UserRepository userRepository=context.getBean(UserRepository.class);
 		User user =new User();
-		user.setName("Pranjal Singh");
-		user.setCity("Delhi");
-		user.setStatus("Java programmer");
+		System.out.println("Enter Name :-");
+		String name=in.readLine();
+		user.setName(name);
+		System.out.println("Enter City :-");
+		String city=in.readLine();
+		user.setCity(city);
+		System.out.println("Enter Status :-");
+		String status=in.readLine();
+		user.setStatus(status);
 		
 		 User user1=userRepository.save(user);
 		 System.out.println(user1);
